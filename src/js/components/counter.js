fbc8fa59db75ec48cd244fff8406bae9e9b73efe
Сергей <serj.elessar@gmail.com>
2023-05-08 12:52:58 +0300

    Update counter.js

--- src/js/components/counter.js
+++ src/js/components/counter.js
@@ -1,61 +1,46 @@
 function counter() {
 	window.addEventListener('load', () => {
 		function digitsCountersInit(digitsCountersItems) {
-			const digitsCounters = digitsCountersItems
-				? digitsCountersItems
-				: document.querySelectorAll('[data-digits-counter]')
-			if (digitsCounters) {
-				digitsCounters.forEach(digitsCounter => {
-					digitsCountersAnimate(digitsCounter)
-				})
-			}
+			const digitsCounters = digitsCountersItems ?? document.querySelectorAll('[data-digits-counter]');
+
+			digitsCounters.forEach(digitsCounter => digitsCountersAnimate(digitsCounter));
 		}
+		
 		function digitsCountersAnimate(digitsCounter) {
-			let startTimestamp = null
-			const duration = parseInt(digitsCounter.dataset.digitsCounter)
-				? parseInt(digitsCounter.dataset.digitsCounter)
-				: 1000
-			const startValue = parseInt(digitsCounter.innerHTML)
-			const startPosition = 0
-			const step = timestamp => {
-				if (!startTimestamp) startTimestamp = timestamp
-				const progress = Math.min(
-					(timestamp - startTimestamp) / duration,
-					1
-				)
-				digitsCounter.innerHTML = Math.floor(
-					progress * (startPosition + startValue)
-				)
-				progress < 1 ? window.requestAnimationFrame(step) : null
+			let startTimestamp;
+			const duration = parseInt(digitsCounter?.dataset?.digitsCounter) ?? 1000;
+			const startValue = parseInt(digitsCounter.innerHTML);
+			const startPosition = 0;
+			
+			const step = (timestamp) => {
+				if (!startTimestamp) startTimestamp = timestamp;
+				
+				const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+				
+				digitsCounter.innerHTML = Math.floor(progress * (startPosition + startValue));
+				progress < 1 && window.requestAnimationFrame(step);
 			}
-			window.requestAnimationFrame(step)
+			
+			window.requestAnimationFrame(step);
 		}
-		const options = {
-			threshold: 0.3
-		}
+		
+		const options = { threshold: 0.3 };
+		
 		const observer = new IntersectionObserver((entries, observer) => {
-			entries.forEach(entry => {
-				if (entry.isIntersecting) {
-					const targetElement = entry.target
-					const digitsCountersItems = targetElement.querySelectorAll(
-						'[data-digits-counter]'
-					)
-					digitsCountersItems.length
-						? digitsCountersInit(digitsCountersItems)
-						: null
-					observer.unobserve(targetElement)
+			entries.forEach(({ isIntersecting, target }) => {
+				if (isIntersecting) {
+					const digitsCountersItems = target.querySelectorAll('[data-digits-counter]');
+					
+					digitsCountersItems.length && digitsCountersInit(digitsCountersItems);
+					observer.unobserve(target);
 				}
 			})
-		}, options)
-		const sections = document.querySelectorAll(
-			'[data-digits-counter-wrapper]'
-		)
-		if (sections.length) {
-			sections.forEach(section => {
-				observer.observe(section)
-			})
-		}
+		}, options);
+		
+		const sections = document.querySelectorAll('[data-digits-counter-wrapper]');
+		
+		sections.forEach(section => observer.observe(section));
 	})
 }
 
-export { counter }
+export { counter };
