411955c593bb8e83fa069863ac2840a402d3242e
nikkeyl <nikkeyl.dev@gmail.com>
2023-05-20 10:16:29 +0300

    refactor: Optimized the 'cssLoaderOptions.js' function

--- config/settings/cssLoaderOptions.js
+++ config/settings/cssLoaderOptions.js
@@ -1,15 +1,13 @@
-const cssLoaderOptions = (importLoaders = 0, sourceMap = false, endPath = '') => {
-    return {
-        importLoaders: importLoaders,
-        sourceMap: sourceMap,
-        modules: false,
-        url: {
-            filter: url => {
-                !url.includes(`content${endPath}`)
-                    || !url.includes(`fonts${endPath}`)
-            }
+const cssLoaderOptions = (importLoaders = 0, sourceMap = false, endPath = '') => ({
+    importLoaders: importLoaders,
+    sourceMap: sourceMap,
+    modules: false,
+    url: {
+        filter: url => {
+            !url.includes(`content${endPath}`)
+                || !url.includes(`fonts${endPath}`)
         }
     }
-}
+})
 
 export { cssLoaderOptions }
