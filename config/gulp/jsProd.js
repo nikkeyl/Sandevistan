d78ef984672117199ddaf74fd4c66b7cd299bd85
nikkeyl <nikkeyl.dev@gmail.com>
2023-06-04 14:33:56 +0300

    refactor: jsProd.js

--- config/gulp/jsProd.js
+++ config/gulp/jsProd.js
@@ -3,11 +3,9 @@ import { app } from '../../gulpfile.js'
 import webPackConfig from '../webpack/webpack.prod.js'
 
 const jsProd = () => {
-	app.gulp.src(app.paths.src.js)
+	return app.gulp.src(app.paths.src.js)
 		.pipe(app.plugins.catchError('JS'))
-		.pipe(app.plugins.webpack({
-			config: webPackConfig
-		}))
+		.pipe(app.plugins.webpack({ config: webPackConfig }))
 		.pipe(app.gulp.dest(app.paths.build.js))
 }
 
