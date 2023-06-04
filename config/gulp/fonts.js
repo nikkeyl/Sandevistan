99e070e6c7fe119d089e2f9bdd5a6666cde56a00
nikkeyl <nikkeyl.dev@gmail.com>
2023-06-04 14:30:01 +0300

    refactor: 'fonts.js' Added support for all styles and 'font-style: italic'

--- config/gulp/fonts.js
+++ config/gulp/fonts.js
@@ -7,10 +7,8 @@ function cb(error) {
 	if (error) {
 		console.log(app.plugins.chalk.bgRed.white.bold('[ERROR]\n'), error)
 	} else {
-		console.log(
-			app.plugins.chalk.bgGreen.white.bold(
-				'[SUCCESS]\nThe (fonts.scss) file is written]'
-			)
+		console.log(app.plugins.chalk.bgGreen.white.bold(
+			'[SUCCESS]\nThe (fonts.scss) file is written]')
 		)
 	}
 }
@@ -18,9 +16,7 @@ function cb(error) {
 const otfToTtf = () => {
 	return app.gulp.src(`${app.paths.srcFolder}/fonts/*.otf`)
 		.pipe(app.plugins.catchError('FONTS'))
-		.pipe(fonter({
-			formats: ['ttf']
-		}))
+		.pipe(fonter({ formats: ['ttf'] }))
 		.pipe(app.gulp.dest(`${app.paths.srcFolder}/fonts/`))
 }
 const ttfToWoff = () => {
@@ -31,7 +27,7 @@ const ttfToWoff = () => {
 		.pipe(app.gulp.src(`${app.paths.srcFolder}/fonts/*.woff2`))
 		.pipe(app.gulp.dest(app.paths.build.fonts))
 }
-const fontStyle = () => {
+const fontsStyles = () => {
 	const fontStylesFile = `${app.paths.srcFolder}/scss/base/fonts/fonts.scss`
 
 	app.plugins.fs.readdir(app.paths.build.fonts, (err, fontFiles) => {
@@ -39,14 +35,21 @@ const fontStyle = () => {
 			if (!app.plugins.fs.existsSync(fontStylesFile)) {
 				const fontWeights = {
 					thin: 100,
+					hairline: 100,
 					extralight: 200,
+					ultralight: 200,
 					light: 300,
 					regular: 400,
 					medium: 500,
 					semibold: 600,
+					demibold: 600,
 					bold: 700,
-					heavy: 800,
+					extrabold: 800,
+					ultrabold: 800,
 					black: 900,
+					heavy: 900,
+					extrablack: 950,
+					ultrablack: 950
 				}
 
 				let newFileOnly
@@ -62,10 +65,13 @@ const fontStyle = () => {
 					if (newFileOnly !== fileName) {
 						const [fontName, fontWeight = 'regular'] = fileName.split('-')
 						const fontWeightValue = fontWeights[fontWeight.toLowerCase()]
+						const fontStyle = fileName.includes('-Italic')
+							? 'italic'
+							: 'normal'
 
 						app.plugins.fs.appendFile(
 							fontStylesFile,
-							`@font-face {\n\tsrc: url("../fonts/${fileName}.woff2") format("woff2");\n\tfont-family: "${fontName}";\n\tfont-weight: ${fontWeightValue};\n\tfont-style: normal;\n\tfont-display: swap;\n}\n\n`,
+							`@font-face {\n\tsrc: url("../fonts/${fileName}.woff2") format("woff2");\n\tfont-family: "${fontName}";\n\tfont-weight: ${fontWeightValue};\n\tfont-style: ${fontStyle};\n\tfont-display: swap;\n}\n\n`,
 							cb
 						)
 						newFileOnly = fileName
@@ -84,4 +90,4 @@ const fontStyle = () => {
 	return app.gulp.src(app.paths.srcFolder)
 }
 
-export { otfToTtf, ttfToWoff, fontStyle }
+export { otfToTtf, ttfToWoff, fontsStyles }
