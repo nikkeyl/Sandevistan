3af869a77a0153cd51ade42d333e652ddf3cc0c3
nikkeyl <nikkeyl.dev@gmail.com>
2023-06-24 15:06:34 +0300

    fix: imports in gallery.js

--- src/js/libs/gallery.js
+++ src/js/libs/gallery.js
@@ -2,23 +2,23 @@
 import { nodeObjects } from '@js/helpers/nodeList'
 import lightGallery from 'lightgallery'
 
-// Import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.min.js'
-// Import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.min.js'
+// import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.min.js'
+// import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.min.js'
 
 import '@scss/vendors/gallery/lightgallery'
 
-// Import '@scss/vendors/gallery/lg-relative-caption'
-// Import '@scss/vendors/gallery/lightgallery-bundle'
-// Import '@scss/vendors/gallery/lg-medium-zoom'
-// Import '@scss/vendors/gallery/lg-fullscreen'
-// Import '@scss/vendors/gallery/lg-thumbnail'
-// Import '@scss/vendors/gallery/lg-autoplay'
-// Import '@scss/vendors/gallery/lg-comments'
-// Import '@scss/vendors/gallery/lg-rotate'
-// Import '@scss/vendors/gallery/lg-video'
-// Import '@scss/vendors/gallery/lg-pager'
-// Import '@scss/vendors/gallery/lg-share'
-// Import '@scss/vendors/gallery/lg-zoom'
+// import '@scss/vendors/gallery/lg-relative-caption'
+// import '@scss/vendors/gallery/lightgallery-bundle'
+// import '@scss/vendors/gallery/lg-medium-zoom'
+// import '@scss/vendors/gallery/lg-fullscreen'
+// import '@scss/vendors/gallery/lg-thumbnail'
+// import '@scss/vendors/gallery/lg-autoplay'
+// import '@scss/vendors/gallery/lg-comments'
+// import '@scss/vendors/gallery/lg-rotate'
+// import '@scss/vendors/gallery/lg-video'
+// import '@scss/vendors/gallery/lg-pager'
+// import '@scss/vendors/gallery/lg-share'
+// import '@scss/vendors/gallery/lg-zoom'
 
 const galleries = document.querySelectorAll('[data-gallery]')
 const galleryItems = []
