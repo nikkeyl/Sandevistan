ba913284c765c5db5122294b49e64d0dab81a4c4
nikkeyl <nikkeyl.dev@gmail.com>
2023-07-03 16:14:39 +0300

    refactor: Move forms to components folder

--- src/js/vendors/range.js
+++ src/js/vendors/range.js
@@ -2,7 +2,7 @@
 import noUiSlider from 'nouislider'
 import wNumb from 'wnumb'
 
-import '@scss/layout/forms/range'
+import '@scss/components/forms/range'
 
 function range() {
 	const priceSlider = document.querySelector('[data-range]')
