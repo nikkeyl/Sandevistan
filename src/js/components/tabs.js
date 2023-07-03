4dfb32801fc446b92b04784baac37d2b6ac8c19b
nikkeyl <nikkeyl.dev@gmail.com>
2023-07-03 11:57:03 +0300

    fix: 'tabs.js' and 'tabs.scss'

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -26,35 +26,35 @@ function tabs() {
 	if (mdQueriesArray && mdQueriesArray.length) {
 		mdQueriesArray.forEach(mdQueriesItem => {
 			mdQueriesItem.matchMedia.addEventListener('change', () => {
-				setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia)
+				setTriggerPosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia)
 			})
-			setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia)
+			setTriggerPosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia)
 		})
 	}
 
-	function setTitlePosition(tabsMediaArray, matchMedia) {
+	function setTriggerPosition(tabsMediaArray, matchMedia) {
 		tabsMediaArray.forEach(tabsMediaItem => {
 			tabsMediaItem = tabsMediaItem.item
 
-			const tabsTitles = tabsMediaItem.querySelector('[data-tabs-titles]')
-			const tabsContent = tabsMediaItem.querySelector('[data-tabs-body]')
+			const tabsTriggers = tabsMediaItem.querySelector('[data-tabs-triggers]')
+			const tabsPanels = tabsMediaItem.querySelector('[data-tabs-panels]')
 
-			let tabsTitleItems = tabsMediaItem.querySelectorAll('[data-tabs-title]')
-			let tabsContentItems = tabsMediaItem.querySelectorAll('[data-tabs-item]')
+			let tabsTriggerItems = tabsMediaItem.querySelectorAll('[data-tabs-trigger]')
+			let tabsPanelsItems = tabsMediaItem.querySelectorAll('[data-tabs-item]')
 
-			tabsTitleItems = Array.from(tabsTitleItems).filter(
+			tabsTriggerItems = Array.from(tabsTriggerItems).filter(
 				item => item.closest('[data-tabs]') === tabsMediaItem
 			)
-			tabsContentItems = Array.from(tabsContentItems).filter(
+			tabsPanelsItems = Array.from(tabsPanelsItems).filter(
 				item => item.closest('[data-tabs]') === tabsMediaItem
 			)
-			tabsContentItems.forEach((tabsContentItem, index) => {
+			tabsPanelsItems.forEach((tabsPanelsItem, index) => {
 				if (matchMedia.matches) {
-					tabsContent.append(tabsTitleItems[index])
-					tabsContent.append(tabsContentItem)
+					tabsPanels.append(tabsTriggerItems[index])
+					tabsPanels.append(tabsPanelsItem)
 					tabsMediaItem.classList.add('tab-spoiler')
 				} else {
-					tabsTitles.append(tabsTitleItems[index])
+					tabsTriggers.append(tabsTriggerItems[index])
 					tabsMediaItem.classList.remove('tab-spoiler')
 				}
 			})
@@ -65,38 +65,38 @@ function tabs() {
 		const tabsBlockIndex = tabsBlock.dataset.tabsIndex
 		const tabsActiveHashBlock = tabsActiveHash[0] === tabsBlockIndex
 
-		let tabsTitles = tabsBlock.querySelectorAll('[data-tabs-titles]>*')
-		let tabsContent = tabsBlock.querySelectorAll('[data-tabs-body]>*')
+		let tabsTriggers = tabsBlock.querySelectorAll('[data-tabs-triggers]>*')
+		let tabsPanels = tabsBlock.querySelectorAll('[data-tabs-panels]>*')
 
 		if (tabsActiveHashBlock) {
-			const tabsActiveTitle = tabsBlock.querySelector(
-				'[data-tabs-titles]>.tab-active'
+			const tabsActiveTrigger = tabsBlock.querySelector(
+				'[data-tabs-triggers]>.tab-active'
 			)
 
-			tabsActiveTitle?.classList.remove('tab-active')
+			tabsActiveTrigger?.classList.remove('tab-active')
 		}
 
-		tabsContent = Array.from(tabsContent).filter(
+		tabsPanels = Array.from(tabsPanels).filter(
 			item => item.closest('[data-tabs]') === tabsBlock
 		)
-		tabsTitles = Array.from(tabsTitles).filter(
+		tabsTriggers = Array.from(tabsTriggers).filter(
 			item => item.closest('[data-tabs]') === tabsBlock
 		)
-		tabsContent.forEach((tabsContentItem, index) => {
-			tabsTitles[index].setAttribute('data-tabs-title', '')
-			tabsContentItem.setAttribute('data-tabs-item', '')
+		tabsPanels.forEach((tabsPanelsItem, index) => {
+			tabsTriggers[index].setAttribute('data-tabs-trigger', '')
+			tabsPanelsItem.setAttribute('data-tabs-item', '')
 			tabsActiveHashBlock && index === tabsActiveHash[1]
-				? tabsTitles[index].classList.add('tab-active')
+				? tabsTriggers[index].classList.add('tab-active')
 				: null
-			tabsContentItem.hidden = !tabsTitles[index].classList.contains('tab-active')
+			tabsPanelsItem.hidden = !tabsTriggers[index].classList.contains('tab-active')
 		})
 	}
 
 	function setTabsStatus(tabsBlock) {
 		const tabsBlockIndex = tabsBlock.dataset.tabsIndex
 
-		let tabsTitles = tabsBlock.querySelectorAll('[data-tabs-title]')
-		let tabsContent = tabsBlock.querySelectorAll('[data-tabs-item]')
+		let tabsTriggers = tabsBlock.querySelectorAll('[data-tabs-trigger]')
+		let tabsPanels = tabsBlock.querySelectorAll('[data-tabs-item]')
 
 		function isTabsAnamate(tabsBlock) {
 			if (tabsBlock.hasAttribute('data-tabs-animate')) {
@@ -107,24 +107,24 @@ function tabs() {
 		const tabsBlockAnimate = isTabsAnamate(tabsBlock)
 		const isHash = tabsBlock.hasAttribute('data-tabs-hash')
 
-		tabsContent = Array.from(tabsContent).filter(
+		tabsPanels = Array.from(tabsPanels).filter(
 			item => item.closest('[data-tabs]') === tabsBlock
 		)
-		tabsTitles = Array.from(tabsTitles).filter(
+		tabsTriggers = Array.from(tabsTriggers).filter(
 			item => item.closest('[data-tabs]') === tabsBlock
 		)
-		tabsContent.forEach((tabsContentItem, index) => {
-			if (tabsTitles[index].classList.contains('tab-active')) {
+		tabsPanels.forEach((tabsPanelsItem, index) => {
+			if (tabsTriggers[index].classList.contains('tab-active')) {
 				tabsBlockAnimate
-					? slideDown(tabsContentItem, tabsBlockAnimate)
-					: (tabsContentItem.hidden = false)
-				isHash && !tabsContentItem.closest('.popup')
+					? slideDown(tabsPanelsItem, tabsBlockAnimate)
+					: (tabsPanelsItem.hidden = false)
+				isHash && !tabsPanelsItem.closest('.popup')
 					? setHash(`tab-${tabsBlockIndex}-${index}`)
 					: null
 			} else if (tabsBlockAnimate) {
-				slideUp(tabsContentItem, tabsBlockAnimate)
+				slideUp(tabsPanelsItem, tabsBlockAnimate)
 			} else {
-				tabsContentItem.hidden = true
+				tabsPanelsItem.hidden = true
 			}
 		})
 	}
@@ -132,25 +132,25 @@ function tabs() {
 	function setTabsAction(e) {
 		const el = e.target
 
-		if (el.closest('[data-tabs-title]')) {
-			const tabTitle = el.closest('[data-tabs-title]')
-			const tabsBlock = tabTitle.closest('[data-tabs]')
+		if (el.closest('[data-tabs-trigger]')) {
+			const tabTrigger = el.closest('[data-tabs-trigger]')
+			const tabsBlock = tabTrigger.closest('[data-tabs]')
 
 			if (
-				!tabTitle.classList.contains('tab-active') &&
+				!tabTrigger.classList.contains('tab-active') &&
 				!tabsBlock.querySelector('.slide')
 			) {
-				let tabActiveTitle = tabsBlock.querySelectorAll(
-					'[data-tabs-title].tab-active'
+				let tabActiveTrigger = tabsBlock.querySelectorAll(
+					'[data-tabs-trigger].tab-active'
 				)
 
-				tabActiveTitle.length
-					? (tabActiveTitle = Array.from(tabActiveTitle).filter(
+				tabActiveTrigger.length
+					? (tabActiveTrigger = Array.from(tabActiveTrigger).filter(
 							item => item.closest('[data-tabs]') === tabsBlock
 					  ))
 					: null
-				removeClasses('[data-tabs-title].tab-active', 'tab-active')
-				tabTitle.classList.add('tab-active')
+				removeClasses('[data-tabs-trigger].tab-active', 'tab-active')
+				tabTrigger.classList.add('tab-active')
 				setTabsStatus(tabsBlock)
 			}
 
