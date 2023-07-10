ef45d263eddd40b9a3a60584ba93f5bb118d4c82
nikkeyl <nikkeyl.dev@gmail.com>
2023-07-10 11:51:58 +0300

    fix: catchErrors.js

--- config/utils/catchErrors.js
+++ config/utils/catchErrors.js
@@ -2,9 +2,11 @@ import plumber from 'gulp-plumber'
 import notify from 'gulp-notify'
 
 const catchError = title =>
-	plumber(notify.onError({
-		title: title,
-		message: 'Error: <%= error.message %>'
-	}))
+	plumber({
+		errorHandler: notify.onError({
+			title: title,
+			message: 'Error: <%= error.message %>'
+		})
+	})
 
 export { catchError }
