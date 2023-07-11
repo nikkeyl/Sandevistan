f0cd9532556bc72708ea25e17526c2982f072b7d
nikkeyl <nikkeyl.dev@gmail.com>
2023-07-11 15:29:25 +0300

    refactor: js files

--- src/js/helpers/hourRange.js
+++
@@ -1,9 +0,0 @@
-function hourRange(start, end, script) {
-	/*! This feature helps you run other functions at specific times */
-	const time = new Date()
-	const now = `${time.getHours()}.${time.getMinutes()}`
-
-	return now >= start && now < end && script()
-}
-
-export { hourRange }
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,2 @@
 // import { addTouchClass } from '@js/helpers/addTouchClass'
-// import { hourRange } from '@js/helpers/hourRange'
 // import { isMobile } from '@js/helpers/isMobile'
